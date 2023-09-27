3d4fbb0de2fc53f945cc81313e5f10f70126cf1e
Nathalia Barbosa <nathaliaferdab@gmail.com>
2023-09-27 09:43:37 -0300

    movi o gráfico para outro componente

--- src/componets/IndicadorCardGraph.tsx
+++ src/componets/IndicadorCardGraph.tsx
@@ -1,4 +1,4 @@
-import Plot from 'react-plotly.js';
+import LineGraph from "./LineGraph";
 
 type IndicadorCardGraphProps = {
     indicador: {
@@ -21,42 +21,7 @@ export default function IndicadorCardGraph({ indicador }: IndicadorCardGraphProp
                     {indicador.nome}
                 </h4>
                 <div className='mb-4'>
-                    <Plot
-                        data={[
-                            {
-                                x: indicador.meses,
-                                y: indicador.meta,
-                                type: 'scatter',
-                                mode: 'lines+markers',
-                                marker: { color: '#E51110' },
-                                name: "Meta",
-                            },
-                            {
-                                x: indicador.meses,
-                                y: indicador.supermeta,
-                                type: 'scatter',
-                                mode: 'lines+markers',
-                                marker: { color: '#7D55EF' },
-                                name: "Supermeta",
-                            },
-                            {
-                                x: indicador.meses,
-                                y: indicador.desafio,
-                                type: 'scatter',
-                                mode: 'lines+markers',
-                                marker: { color: '#5EE0F1' },
-                                name: "Desafio",
-                            },
-                        ]}
-                        layout={{
-                            width: 450,
-                            height: 300,
-                            showlegend: false,
-                            yaxis: { showticklabels: false },
-                            xaxis: { showgrid: false },
-                            modebar: { remove: ["hoverCompareCartesian", "hovercompare", "lasso2d", "orbitRotation", "pan2d", "pan3d", "resetCameraDefault3d", "resetCameraLastSave3d", "resetGeo", "resetScale2d", "resetViewMapbox", "resetViews", "select2d", "sendDataToCloud", "tableRotation", "toImage", "toggleHover", "toggleSpikelines", "togglehover", "togglespikelines", "zoom2d", "zoom3d", "zoomIn2d", "zoomInGeo", "zoomInMapbox", "zoomOut2d", "zoomOutGeo", "zoomOutMapbox"] }
-                        }}
-                    />
+                    <LineGraph indicador={indicador} />
                 </div>
             </div>
         </button>
