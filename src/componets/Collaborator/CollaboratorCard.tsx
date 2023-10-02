f31039ee9c842db0799b541717aed9efd2a039a8
Artur Carvalho dos Santos <arturcs2001@gmail.com>
2023-10-02 14:10:43 -0300

    feat: card do perfil do colaborador

--- src/componets/Collaborator/CollaboratorCard.tsx
+++ src/componets/Collaborator/CollaboratorCard.tsx
@@ -12,12 +12,50 @@ interface CollaboratorCardProps {
 export default function CollaboratorCard({ nome, cargo, imagem, email, telefone }: CollaboratorCardProps) {
 
     // TODO: forma de calcular a nota do colaborador
-    const nota = 10;
+    const nota = 3;
+
+    const notaEstrelas = () => {
+        let estrelas = [];
+        for (let i = 0; i < nota; i++) {
+            estrelas.push(<img src="/src/assets/star.svg" alt="Estrela" className="w-4 h-4" />);
+        }
+        return estrelas;
+    }
 
     return (
         <>
-            <div className="bg-white max-w-[33.875rem] w-[33.875rem] rounded-17 border border-cinza-300">
-                <p>{nome}</p>
+            <div className="flex bg-white max-w-[33.875rem] w-[33.875rem] px-6 py-9 gap-7 rounded-17 border border-cinza-300">
+                <img src={imagem} alt="Foto de perfil" className="w-36 h-36 bg-cover"/>
+                <div className="w-full">
+                    <div className="mb-6">
+                        <p className="font-bold text-32">{nome}</p>
+                        <div className="flex items-center gap-2">
+                            <p className="font-semibold text-cinza">{cargo}</p>
+                            <div className="flex gap-1">
+                                {notaEstrelas()}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="border-t border-cinza-100"></div>
+                    <div className="flex mt-6 gap-[3.8rem]">
+                        <div className="flex flex-col gap-[0.625rem]">
+                            <div className="flex gap-3">
+                                <img src="/src/assets/phone.svg" alt="Telefone" />
+                                <p className="font-medium text-base">Telefone</p>
+                            </div>
+                            <p className="font-medium text-cinza text-sm">{telefone}</p>
+                        </div>
+                        <div className="flex flex-col gap-[0.625rem] ml-6">
+                            <div className="flex gap-3">
+                                <img src="/src/assets/mail.svg" alt="E-mail" />
+                                <p className="font-medium text-base">E-mail</p>
+                            </div>
+                            <p className="font-medium text-cinza text-sm">{email}</p>
+                        </div>
+                    </div>
+                    
+                </div>
+                
             </div>
         </>
     )
