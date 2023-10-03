2ee4b97943a414296876b4a0de6d1cc372abc01a
Nathalia Barbosa <nathaliaferdab@gmail.com>
2023-10-02 23:55:01 -0300

    (fix): Melhorado aparência geral + estrelinhas

--- src/pages/Collaborators.tsx
+++ src/pages/Collaborators.tsx
@@ -5,6 +5,7 @@ import { collaboratorType } from "../types"
 import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Api from "../Api";
+import starSvg from '../assets/star.svg';
 
 
 export default function Collaborators() {
@@ -15,7 +16,6 @@ export default function Collaborators() {
     const loadTableData = () => {
         let newData = allCollaborators.map((collaborator) => {
             const year = new Date(collaborator.data_admissao).getFullYear();
-            console.log(collaborator)
             return {
                 colaborador: collaborator,
                 membro: `desde ${year}`,
@@ -28,7 +28,9 @@ export default function Collaborators() {
         setTableData(newData);
     };
 
-    async function loadNota(colaboradores: collaboratorType[]): Promise<any> {
+    async function loadNotas(colaboradores: collaboratorType[]): Promise<any> {
+        arrayNotas = [] as { colaborador: collaboratorType, nota: number }[];
+
         for (let i = 0; i < colaboradores.length; i++) {
             const currMonth = new Date().getMonth() + 1;
             const currYear = new Date().getFullYear();
@@ -45,9 +47,20 @@ export default function Collaborators() {
         }
     };
 
+    const notaEstrelas = (nota: number) => {
+        let estrelas = [];
+        for (let i = 0; i < nota; i++) {
+            estrelas.push(<img src={starSvg} alt="Estrela" className="w-4 h-4" key={i} />);
+        }
+        if (nota === 0) {
+            estrelas.push(<p className='text-gray-500 text-sm'>Sem nota</p>);
+        }
+        return estrelas;
+    };
+
     useEffect(() => {
         const loadData = async () => {
-            await loadNota(allCollaborators);
+            await loadNotas(allCollaborators);
             loadTableData();
         }
 
@@ -64,10 +77,10 @@ export default function Collaborators() {
                         <Header />
                     </div>
 
-                    <div className='flex flex-col items-center w-[85%] mx-10'>
+                    <div className='flex flex-col justify-between items-center w-[85%] h-full mx-10'>
 
                         <div className='bg-vermelho w-full h-[2.75rem] flex items-center justify-start rounded-xl'>
-                            <div className='flex flex-row items-center justify-around w-[70%]'>
+                            <div className='flex flex-row items-center justify-around w-[80%]'>
                                 <p className='text-white font-bold'>Colaborador</p>
                                 <p className='text-white font-bold'>Colocação</p>
                                 <p className='text-white font-bold'>Membro</p>
@@ -76,10 +89,10 @@ export default function Collaborators() {
                         </div>
 
                         {tableData.length > 0 ? (
-                            <ul className='bg-branco w-full flex flex-col divide-y divide-gray-200'>
+                            <ul className='w-full flex flex-col divide-y divide-gray-200 h-[90%] rounded-xl overflow-y-scroll'>
                                 {tableData.map((colaborador, index) => (
-                                    <li key={index} className='flex flex-row items-center justify-start w-full h-min my-2'>
-                                        <div className='w-[70%] flex justify-around items-center'>
+                                    <li key={index} className='flex flex-row items-center justify-start w-full h-min my-2 relative'>
+                                        <div className='w-[80%] flex justify-around items-center'>
                                             <div className='flex flex-row items-center mt-2 w-[30%]'>
                                                 <img src={colaborador.colaborador.imagem} alt="Foto do colaborador" className='w-[2.5rem] h-[2.5rem] rounded-full mr-2' />
                                                 <div className='flex flex-col items-start justify-center'>
@@ -88,12 +101,16 @@ export default function Collaborators() {
                                                 </div>
                                             </div>
 
-                                            <p className='text-gray-500 w-[10%] text-center'>{index + 1}</p>
+                                            <p className='text-gray-500 w-[10%] text-center'>#{index + 1}</p>
                                             <p className='text-gray-500 w-[25%] text-center'>{colaborador.membro}</p>
-                                            <p className='text-gray-500 w-[15%] text-center'>{colaborador.status}</p>
+                                            <div className='flex flex-row items-center justify-center w-[15%]'>
+                                                {notaEstrelas(colaborador.status).map((estrela, index) => (
+                                                    <span key={index}>{estrela}</span>
+                                                ))}
+                                            </div>
                                         </div>
 
-                                        <Link to={`/collaborators/${colaborador.colaborador.id}`} className='ml-32'>
+                                        <Link to={`/collaborators/${colaborador.colaborador.id}`} className='absolute right-0 bottom-[10%] mr-16'>
                                             <button className='text-vermelho text-sm border-2 border-vermelho rounded-xl px-4 py-1 hover:bg-vermelho hover:text-white transition duration-300 ease-in-out'>
                                                 Ver Perfil
                                             </button>
@@ -105,11 +122,9 @@ export default function Collaborators() {
                             <p className='my-2 text-gray-500 text-2xl font-bold text-center'>Carregando...</p>
                         )}
 
-                        <Link to='/new_collaborator' className="flex justify-end w-full mt-6 mr-4">
-                            <button className='bg-vermelho text-white text-sm rounded-md px-6 py-2'>
-                                Add Colaborador
-                            </button>
-                        </Link>
+                        <button className='bg-vermelho text-white text-sm rounded-md px-10 py-2 mt-6 mr-4 items-center w-max hover:bg-vermelho-700 transition duration-300 ease-in-out'>
+                            <Link to='/new_collaborator'>Add Colaborador</Link>
+                        </button>
                     </div>
                 </div>
             </div>
