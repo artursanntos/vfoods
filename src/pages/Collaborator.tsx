f2bb19c9b0fd681273ad4979e3a6994a8969c228
Artur Carvalho dos Santos <arturcs2001@gmail.com>
2023-10-03 16:20:06 -0300

    feat: inicializando cardGraph do collab + estrelinhas

--- src/pages/Collaborator.tsx
+++ src/pages/Collaborator.tsx
@@ -4,13 +4,15 @@ import { SideBar } from "../componets/SideBar/SideBar";
 import { useEffect, useState } from "react";
 import Api from "../Api";
 import CollaboratorCard from "../componets/Collaborator/CollaboratorCard";
-import { collaboratorType } from "../types";
+import { colaboratorIndicatorType, collaboratorType } from "../types";
 import IndicatorsList from "../componets/Collaborator/IndicatorsList";
+import CollabCardGraph from "../componets/CollabCardGraph";
 
 export default function Collaborator() {
 
     const params = useParams();
     const [collab, setCollab] = useState<collaboratorType>({} as collaboratorType);
+    const [monthData, setMonthData] = useState<colaboratorIndicatorType[]>([] as colaboratorIndicatorType[]); 
     const collabId = params.id;
 
     const getCollaboratorData = () => {
@@ -20,8 +22,25 @@ export default function Collaborator() {
         })
     }
 
+    const getSemesterData = () => {
+        setMonthData([] as colaboratorIndicatorType[]);
+        const actualMonth = new Date().getMonth() + 1;
+        const firstMonth = actualMonth - 5;
+        const year = new Date().getFullYear();
+        for (let i = firstMonth; i <= actualMonth; i++) {
+            const monthString = i < 10 ? `0${i}` : `${i}`;
+            const url = `/colaborador-indicador/findAllOfColaboratorByMonth/` + collabId + `/` + year + '-' + monthString + `-01T00:00:00.000Z`
+
+            Api.get(url).then((response) => {
+                console.log(response.data.colaboradorIndicadores)
+                setMonthData([...monthData, response.data.colaboradorIndicadores]);
+            })
+        }
+    }
+
     useEffect(() => {
         getCollaboratorData();
+        getSemesterData();
     }, []);
 
     return (
@@ -40,8 +59,12 @@ export default function Collaborator() {
 
                     <div className="w-full h-[calc(100vh-14.25rem)] max-h-[calc(100vh-14.25rem)]">
                         <div className="flex gap-44 h-full w-full justify-center">
-                            <div>
-                                <CollaboratorCard nome={collab.nome} cargo={collab.cargo} imagem={collab.imagem} email={collab.email} telefone={collab.telefone}/>
+                            <div className="flex flex-col justify-between items-center">
+                                <CollaboratorCard id={collabId} nome={collab.nome} cargo={collab.cargo} imagem={collab.imagem} email={collab.email} telefone={collab.telefone}/>
+                                <div className="">
+                                    <CollabCardGraph collab={collab}/>
+                                </div>
+                                
                             </div>
                             <div className="h-full min-h-full max-h-full">
                                 <IndicatorsList id={collabId}/>
