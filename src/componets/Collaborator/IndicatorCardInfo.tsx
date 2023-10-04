b674b99d0b240c14ffd25d64034a79d63c973cf0
Lorena Vilaça <lcjbv2@cin.ufpe.br>
2023-10-04 14:43:33 -0300

    feature(resultadoIndicador): rendering colaborator name and indicator unit

--- src/componets/Collaborator/IndicatorCardInfo.tsx
+++ src/componets/Collaborator/IndicatorCardInfo.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { colaboratorIndicatorType } from "../../types";
+import Api from "../../Api";
 
 interface IndicatorCardInfoProps {
     indicatorInfo: colaboratorIndicatorType;
@@ -7,17 +8,47 @@ interface IndicatorCardInfoProps {
 
 export default function IndicatorCardInfo({ indicatorInfo }: IndicatorCardInfoProps) {
 
-    const [editIsOpen, setEditIsOpen] = useState(false);
+    const [indicatorUnit, setIndicatorUnit] = useState<string>('');
+    const [colaboratorName, setColaboratorName] = useState<string>('');
+
+    const getIndicatorUnit = () => {
+        const url = `/indicador/info/byId/${indicatorInfo.idIndicador}`
+        Api.get(url).then((response) => {
+            setIndicatorUnit(response.data.unidade_medida);
+        })
+    }
+
+
+    const getColaboratorName = () => {
+        const url = `/colaborador/${indicatorInfo.idColaborador}`
+        Api.get(url).then((response) => {
+            console.log(response.data)
+            setColaboratorName(response.data.nome);
+        })
+    }
+
+    const getProgressValue = () => {
+        const resultado = indicatorInfo.resultado;
+        const total = indicatorInfo.desafio;
+        const progresso = (resultado / total) * 100;
+        //console.log(progresso);
+        return progresso;
+    }
+
+    useEffect(() => {
+        getIndicatorUnit();
+        getColaboratorName();
+    }, [])
 
     return (
-        <section className="max-w-fit min-w-[300px]
+        <section className="max-w-fit min-w-[325px]
         bg-slate-50
          rounded-17 drop-shadow-md
          align-middle">
             <div className="px-7 py-5" >
                 <p className="font-semibold text-xl text-cinza-400">Meta</p>
-                <p className="text-cinza-400"> &gt;unidade de medida: </p>
-                <p className="text-cinza-700"> "Fulano" atingiu...</p>
+                <p className="text-cinza-400"> &gt;unidade de medida: {indicatorUnit} </p>
+                <p className="text-cinza-700"> {colaboratorName} atingiu...</p>
                 <div className="flex flex-row justify-evenly py-4 ">
                     <div className="flex flex-col flex-1 w-1/3 items-center">
                         <label className="font-semibold">
