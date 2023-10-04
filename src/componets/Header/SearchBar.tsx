392514a5d5f02bec12013f63fa59e9295fe55643
Nathalia Barbosa <nathaliaferdab@gmail.com>
2023-10-03 21:23:31 -0300

    (fix): Melhorias diversas

--- src/componets/Header/SearchBar.tsx
+++ src/componets/Header/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from 'react';
+import { useContext, useState, useEffect, useRef } from 'react';
 import { VfoodsContext } from '../../contexts/VfoodsContext';
 import iconSearch from './assets/IconSearch.png';
 import { collaboratorType, indicatorType } from '../../types';
@@ -6,49 +6,63 @@ import { Link } from 'react-router-dom';
 
 export function SearchBar() {
     const { allCollaborators, allIndicators } = useContext(VfoodsContext);
-    const [search, setSearch] = useState('');
+    const searchInputRef = useRef<HTMLInputElement | null>(null);
     const [filteredCollaborators, setFilteredCollaborators] = useState([] as collaboratorType[]);
     const [filteredIndicators, setFilteredIndicators] = useState([] as indicatorType[]);
     const [showResults, setShowResults] = useState(false);
+    const [searchValue, setSearchValue] = useState('');
 
-    const handleShowResults = () => {
-        setShowResults(true);
-    }
+    const filterItems = (items: any, search: string) => {
+        return items
+            .filter((item: any) => item.nome.toLowerCase().includes(search.toLowerCase()))
+            .sort((a: any, b: any) => a.nome.localeCompare(b.nome));
+    };
 
-    useEffect(() => {
+    const handleSearch = (search: string) => {
+        setSearchValue(search);
+
         if (search === '') {
-            // Se o campo de pesquisa estiver vazio, oculte os resultados
             setShowResults(false);
             return;
         }
 
-        let fc = allCollaborators.filter((collaborator) => {
-            return collaborator.nome.toLowerCase().includes(search.toLowerCase());
-        });
+        setShowResults(true);
 
-        let fi = allIndicators.filter((indicator) => {
-            return indicator.nome.toLowerCase().includes(search.toLowerCase());
-        });
+        let fc = filterItems(allCollaborators, search);
+        let fi = filterItems(allIndicators, search);
 
         setFilteredCollaborators(fc);
         setFilteredIndicators(fi);
-    }, [search, allCollaborators, allIndicators]);
+    }
+
+    const handleClickOutside = (event: any) => {
+        if (searchInputRef.current && !searchInputRef.current.contains(event.target)) {
+            setShowResults(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('click', handleClickOutside);
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
 
     return (
         <div className='w-[32.15rem] flex flex-col'>
             <div className="border border-cinza-300 rounded-19 h-[3.5rem] w-[32.15rem]">
                 <input
                     type="text"
+                    ref={searchInputRef}
                     className="float-left bg-transparent pl-[2.25rem] pt-4 mr-[3rem] italic text-lg border-none w-[24rem] placeholder:text-cinza-300 focus:shadow-none outline-none"
                     placeholder="Pesquise colaboradores e indicadores"
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
+                    value={searchValue}
+                    onChange={(e) => handleSearch(e.target.value)}
                 />
                 <img
                     src={iconSearch}
                     className="w-[2.75rem] pt-[0.3rem] cursor-pointer"
                     alt="Buscar"
-                    onClick={handleShowResults} // Adicione um evento de clique para iniciar a pesquisa
                 />
             </div>
 
